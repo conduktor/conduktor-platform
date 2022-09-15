1378312fa61c35ef2c65e79fcd3367f522468e6d
rokh-conduktor <roch@conduktor.io>
2022-09-15 15:33:47 +0200

    customizable admin

--- example-local/run-local.sh
+++ example-local/run-local.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 set -eu
 
+read -p 'Organisation name: ' ORGANISATION_NAME
+read -p 'Admin email: ' ADMIN_EMAIL
+read -p 'Admin password: ' ADMIN_PSW
+
+
+export ORGANISATION_NAME
+export ADMIN_EMAIL
+export ADMIN_PSW
+
 SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 
 if [[ "$#" == 0 ]]; then
