e064d21e9970eeb3e2c30792cc9bc27eb70629bb
Florent Ramiere <florent@ramiere.com>
2022-09-25 13:47:17 +0200

    cosmetics

--- example-local/autorun/autorun.sh
+++ example-local/autorun/autorun.sh
@@ -97,7 +97,7 @@ verify_installed()
 {
   local cmd="$1"
   if [[ $(type $cmd 2>&1) =~ "not found" ]]; then
-    echo -e "\nERROR: This script requires '$cmd'. Please install '$cmd' and run again.\n"
+    echo "❌ This script requires '$cmd'. Please install '$cmd' and run again.\n"
     exit 1
   fi
   return 0
